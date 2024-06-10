63e4d144a71abcc2198981d6e9a58d6de9f6e1ed
IgorDGomes <igordgomes@gmail.com>
2024-06-10 23:22:29 +0100

    Add mobile app page mockup

--- src/app/mobile-app/page.tsx
+++ src/app/mobile-app/page.tsx
@@ -1,4 +1,7 @@
 import { Metadata } from 'next'
+import Image from 'next/image'
+
+import Temp from "@/assets/Instagram.svg"
 
 export const metadata: Metadata = {
     title: 'OLB | Mobile App'
@@ -6,8 +9,47 @@ export const metadata: Metadata = {
 
 export default function MobileApp() {
     return (
-        <>
+        <div className='flex flex-col gap-10 lg:max-w-[1440px] mx-auto pb-10'>
+            <div className='grid grid-cols-2'>
+                <div className='flex flex-col items-center justify-center lg:max-w-[550px]'>
+                    <h1>Mobile App</h1>
+                    <p>Description</p>
+                </div>
+                <div className='relative'>
+                    <Image src={Temp} alt='' className='absolute top-0 right-0 left-0' />
+                </div>
+            </div>
+            <div className='flex flex-col gap-8 items-center justify-center'>
+                <h2>Subtitle</h2>
+                <div className='grid grid-cols-3 w-full'>
+                    <div className='flex flex-col items-center justify-center'>
+                        <Image src={Temp} alt='' />
+                        <h3>Subtitle</h3>
+                        <p>Description</p>
+                    </div>
+                    <div className='flex flex-col items-center justify-center'>
+                        <Image src={Temp} alt='' />
+                        <h3>Subtitle</h3>
+                        <p>Description</p>
+                    </div>
+                    <div className='flex flex-col items-center justify-center'>
+                        <Image src={Temp} alt='' />
+                        <h3>Subtitle</h3>
+                        <p>Description</p>
+                    </div>
+                </div>
+            </div>
             <div></div>
-        </>
+            <div className='flex flex-col items-center justify-center gap-4'>
+                <div className='flex flex-col items-center'>
+                    <h2 className='pb-2'>Subtitle</h2>
+                    <p>Description</p>
+                </div>
+                <div className='flex gap-10'>
+                    <button type="button">Download for Apple</button>
+                    <button type="button">Download for Android</button>
+                </div>
+            </div>
+        </div>
     )
 }
